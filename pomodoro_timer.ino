a029d68aa76f7a8bef92d5a17d826920ba4c0343
Matthew Grogan <mvgnyc@gmail.com>
2017-11-12 11:37:34 -0500

    First timer

--- pomodoro_timer.ino
+++ pomodoro_timer.ino
@@ -1,9 +1,39 @@
+/*******************
+ *  Pomodoro Timer
+ *  Matthew Grogan
+ *******************/
+
+#define MATRIX_I2C_ADDR 0x70
+int ACTIVE_SECS = 25 * 60;
+
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+unsigned long start_time;
+unsigned long elapsed;
+unsigned long remaining;
+
 void setup() {
-  // put your setup code here, to run once:
+  matrix.begin(MATRIX_I2C_ADDR);
+
+  start_time = millis();
+
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  elapsed = (millis() - start_time) / 1000;
+  remaining = ACTIVE_SECS - elapsed;
+
+  Serial.println(remaining);
+  
+
+  matrix.println(remaining);
+  matrix.drawColon(false);
+  matrix.writeDisplay();
+  delay(1000);
 
 }
